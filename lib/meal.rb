cd25fa653dad28e2b7b2ddcd53997cef8e7a6ff0
Hannah Malm <hannahemalm616@gmail.com>
2021-02-24 23:42:36 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,7 +1,17 @@
 class Meal
-  def meals
-    Meal.all.select do |meal|
-      meal.customer == self
-  end     
+  attr_accessor :waiter, :customer, :total, :tip 
+  @@all = []
+  
+  def initialize(waiter,customer,total,tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all
+  end   
 end 
 end
