4b5b424d9308b0192534f19788288a45ae7e25d6
Hannah Malm <hannahemalm616@gmail.com>
2021-02-24 23:37:44 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,7 @@
 class Meal
-
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+  end     
+end 
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,19 @@
 class Waiter
-
-end
+  attr_accessor :name, :yrs_experience
+  @@all = []
+  
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  #This method will allow us to create new meals as a customer and associate each new meal with the customer that created it 
+  def new_meal(customer,total, tip=0)
+    Meal.new(self,customer,total,tip)
+  end   
+end   
